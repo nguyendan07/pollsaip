5ff9aad9dbae1e0a70ffa4aab2729b0bf418b540
dannv6 <dannv6@fpt.com.vn>
2019-06-23 17:07:12 +0700

    API with pure Django

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.conf import settings
 
-# Create your views here.
+from .models import Choice, Question
+
+
+def index(request):
+    question = Question.objects.all()[:settings.MAX_OBJECTS]
+    data = {
+        'results': list(question.values('question_text', 'created_by__username', 'pub_date'))
+    }
+    return JsonResponse(data)
+
+
+def detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    data = {
+        'results': {
+            'question': question.question_text,
+            'created_by': question.created_by.username,
+            'pub_date': question.pub_date
+        }
+    }
+    return JsonResponse(data)
