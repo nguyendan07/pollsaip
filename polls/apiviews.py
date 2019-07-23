b49fd865e42f1ca5571c4d8f2a8e49efe4dc2f2d
dannv6 <dannv6@fpt.com.vn>
2019-07-23 23:03:03 +0700

    Using DRF generic views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,21 +1,23 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.conf import settings
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Question, Choice
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, ChoiceSerializer, VoteSerializer
 
 
-class QuestionList(APIView):
-    def get(self, request):
-        question = Question.objects.all()[:settings.MAX_OBJECTS]
-        data = QuestionSerializer(question, many=True).data
-        return Response(data)
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
 
-class QuestionDetail(APIView):
-    def get(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        data = QuestionSerializer(question).data
-        return Response(data)
+class QuestionDetail(generics.RetrieveDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,8 @@ from . import apiviews
 
 urlpatterns = [
     # api
-    path('', apiviews.QuestionList.as_view(), name='question_list'),
-    path('<int:pk>/', apiviews.QuestionDetail.as_view(), name='question_detail')
+    path('polls', apiviews.QuestionList.as_view(), name='question_list'),
+    path('poll/<int:pk>/', apiviews.QuestionDetail.as_view(), name='question_detail'),
+    path('choices/', apiviews.ChoiceList.as_view(), name='choice_list'),
+    path('vote/', apiviews.CreateVote.as_view(), name='create_vote')
 ]
